401cff523c555b0dff6b8d538712ce59a4e45478
XelXen <xelte.xynos@gmail.com>
2021-12-06 21:38:54 +0530

    Latest Config

--- rWrite.py
+++ rWrite.py
@@ -8,47 +8,101 @@
 device_name = "generic"
 
 # Device codename (Default = generic)
-device_codename = ""
+device_codename = "generic"
 
 # Device Architecture (arm or arm64) (Default = arm64)
 device_architecture = ""
 
 # Block Details
-before_part1_size = "" # Partition size Before Partition 1
-block_type = "" # Block Type (emmc or SSD)
+max_part_number = "" # Maximum Partition Number
+disk_path = "" # Path to the disk
 
 # Partition 1
 part1 = "" # True or False
+before_part1_size = "" # Partition size Before Partition 1
+part1_number = "" # Partition Number
 part1_name = "" # Partition Name
+part1_label = "" # Partition Label
+part1_file_system = "" # Partition File System
 min_part1_size = "" # Minimum Partition Size
 max_part2_size = "" # Maximum Partition Size
 
 # Partition 2
-part2 = ""
-part2_name  = ""
-min_part2_size = ""
-max_part2_size = ""
+part2 = "" # True or False
+before_part2_size = "" # Partition size Before Partition 2
+part2_number = "" # Partition Number
+part2_name  = "" # Partition Name
+part2_label = "" # Partition Label
+part2_file_system = "" # Partition File System
+min_part2_size = "" # Minimum Partition Size
+max_part2_size = "" # Maximum Partition Size
 
 # Partition 3
-part3 = ""
-part3_name  = ""
-min_part3_size = ""
-max_part3_size = ""
+part3 = "" # True or False
+before_part3_size = "" # Partition size Before Partition 3
+part3_number = "" # Partition Number
+part3_name  = "" # Partition Name
+part3_label = "" # Partition Label
+part3_file_system = "" # Partition File System
+min_part3_size = "" # Minimum Partition Size
+max_part3_size = "" # Maximum Partition Size
 
 # Partition 4
-part4 = ""
-part4_name  = ""
-min_part4_size = ""
-max_part4_size = ""
+part4 = "" # True or False
+before_part4_size = "" # Partition size Before Partition 4
+part4_number = "" # Partition Number
+part4_name  = "" # Partition Name
+part4_label = "" # Partition Label
+part4_file_system = "" # Partition File System
+min_part4_size = "" # Minimum Partition Size
+max_part4_size = "" # Maximum Partition Size
 
 # Partition 5
-part5 = ""
-part5_name  = ""
-min_part5_size = ""
-max_part5_size = ""
+part5 = "" # True or False
+before_part5_size = "" # Partition size Before Partition 5
+part5_number = "" # Partition Number
+part5_name  = "" # Partition Name
+part5_label = "" # Partition Label
+part5_file_system = "" # Partition File System
+min_part5_size = "" # Minimum Partition Size
+max_part5_size = "" # Maximum Partition Size
 
 # Partition 6
-part6 = ""
-part6_name  = ""
-min_part6_size = ""
-max_part6_size = ""
+part6 = "" # True or False
+before_part6_size = "" # Partition size Before Partition 6
+part6_number = "" # Partition Number
+part6_name  = "" # Partition Name
+part6_label = "" # Partition Label
+part6_file_system = "" # Partition File System
+min_part6_size = "" # Minimum Partition Size
+max_part6_size = "" # Maximum Partition Size
+
+# Partition 7
+part7 = "" # True or False
+before_part7_size = "" # Partition size Before Partition 7
+part7_number = "" # Partition Number
+part7_name  = "" # Partition Name
+part7_label = "" # Partition Label
+part7_file_system = "" # Partition File System
+min_part7_size = "" # Minimum Partition Size
+max_part7_size = "" # Maximum Partition Size
+
+# Partition 8
+part8 = "" # True or False
+before_part8_size = "" # Partition size Before Partition 8
+part8_number = "" # Partition Number
+part8_name  = "" # Partition Name
+part8_label = "" # Partition Label
+part8_filesystem = "" # Partition Filesystem
+min_part8_size = "" # Minimum Partition Size
+max_part8_size = "" # Maximum Partition Size
+
+# Partition 9
+part9 = "" # True or False
+before_part9_size = "" # Partition size Before Partition 9
+part9_number = "" # Partition Number
+part9_name  = "" # Partition Name
+part9_label = "" # Partition Label
+part9_filesystem = "" # Partition Filesystem
+min_part9_size = "" # Minimum Partition Size
+max_part9_size = "" # Maximum Partition Size
