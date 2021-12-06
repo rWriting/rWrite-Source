ce6d69be662dd360b89a588e3708e272863562e6
XelXen <xelte.xynos@gmail.com>
2021-12-06 11:20:25 +0530

    Create Initial Config

---
+++ rWrite.py
@@ -0,0 +1,54 @@
+# rWrite Config 1.0
+# Author : @XelXen
+
+# Device : Redmi Note 6 Pro
+# Maintainer : @XelXen
+
+# Name of the device (Default = rWrite Generic)
+device_name = ""
+
+# Device codename (Default = generic)
+device_codename = ""
+
+# Device Architecture (arm or arm64) (Default = arm64)
+device_architecture = ""
+
+# Block Details
+before_part1_size = "" # Partition size Before Partition 1
+block_type = "" # Block Type (emmc or SSD)
+
+# Partition 1
+part1 = "" # True or False
+part1_name = "" # Partition Name
+min_part1_size = "" # Minimum Partition Size
+max_part2_size = "" # Maximum Partition Size
+
+# Partition 2
+part2 = ""
+part2_name  = ""
+min_part2_size = ""
+max_part2_size = ""
+
+# Partition 3
+part3 = ""
+part3_name  = ""
+min_part3_size = ""
+max_part3_size = ""
+
+# Partition 4
+part4 = ""
+part4_name  = ""
+min_part4_size = ""
+max_part4_size = ""
+
+# Partition 5
+part5 = ""
+part5_name  = ""
+min_part5_size = ""
+max_part5_size = ""
+
+# Partition 6
+part6 = ""
+part6_name  = ""
+min_part6_size = ""
+max_part6_size = ""
